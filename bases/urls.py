9396627782d5010805d591d7f90bf40032dfe561
Daniel Bojorge <debs.consultores@gmail.com>
2019-04-27 01:10:07 -0600

    Login/Logout

--- bases/urls.py
+++ bases/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from bases.views import Home
 
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'),
+        name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='bases/login.html'),
+         name='logout'),
 ]
--- bases/views.py
+++ bases/views.py
@@ -6,4 +6,4 @@ from django.views import generic
 
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
-    login_url='/admin'
+    login_url='bases:login'
