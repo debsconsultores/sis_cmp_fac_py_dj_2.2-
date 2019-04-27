308cae0791a34b123e27d22b08eb3bb0e7c9e123
Daniel Bojorge <debs.consultores@gmail.com>
2019-04-27 00:24:37 -0600

    Home con LoginRequiredMixin

--- bases/views.py
+++ bases/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
 
-class Home(generic.TemplateView):
+class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
+    login_url='/admin'
