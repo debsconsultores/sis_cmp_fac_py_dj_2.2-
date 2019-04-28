fab516e1762ff5f7751dfab3d6f12cc8feef89ae
Daniel Bojorge <debs.consultores@gmail.com>
2019-04-27 23:44:20 -0600

    Categoria - Modelo

--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo
+
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Categoría',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).save()
+
+    class Meta:
+        verbose_name_plural= "Categorias"
