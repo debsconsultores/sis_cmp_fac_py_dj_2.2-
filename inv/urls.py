8ff5dddd6794cc99b2f8b2975a8e47d9d105b9a3
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-01 01:54:10 -0600

    Categoria - Edit - Rutas

--- inv/urls.py
+++ inv/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CategoriaView, CategoriaNew
+from .views import CategoriaView, CategoriaNew, CategoriaEdit
 
 urlpatterns = [
     path('categorias/',CategoriaView.as_view(), name='categoria_list'),
     path('categorias/new',CategoriaNew.as_view(), name='categoria_new'),
+    path('categorias/edit/<int:pk>',CategoriaEdit.as_view(), name='categoria_edit'),
 ]
