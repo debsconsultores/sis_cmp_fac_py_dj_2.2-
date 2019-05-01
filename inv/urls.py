47577baaca5d2b68ff896300bca6d9e68a14021c
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-01 01:22:26 -0600

    Categoria - New - Ruta

--- inv/urls.py
+++ inv/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CategoriaView
+from .views import CategoriaView, CategoriaNew
 
 urlpatterns = [
     path('categorias/',CategoriaView.as_view(), name='categoria_list'),
+    path('categorias/new',CategoriaNew.as_view(), name='categoria_new'),
 ]
