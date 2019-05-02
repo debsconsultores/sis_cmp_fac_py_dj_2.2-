e263dca6e0884eb8cfe675d20f12e35820c909bb
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-01 22:50:10 -0600

    Categoria - Delete - Vista

--- inv/views.py
+++ inv/views.py
@@ -39,3 +39,9 @@ class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         return super().form_valid(form)
+
+class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model=Categoria
+    template_name='inv/catalogos_del.html'
+    context_object_name='obj'
+    success_url=reverse_lazy("inv:categoria_list")
