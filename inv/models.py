324f475dbda02b7e9615a32c5a8f8f0d581af154
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-02 22:18:46 -0600

    SubCategoria - Modelo

--- inv/models.py
+++ inv/models.py
@@ -18,3 +18,22 @@ class Categoria(ClaseModelo):
 
     class Meta:
         verbose_name_plural= "Categorias"
+
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la Categoría'
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion,self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(SubCategoria, self).save()
+
+    class Meta:
+        verbose_name_plural= "Sub Categorias"
+        unique_together = ('categoria','descripcion')
