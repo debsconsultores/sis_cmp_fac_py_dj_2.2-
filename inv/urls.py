6e4c973d312a77c6bdfffb1528992e7d48d8d3a1
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-02 22:46:02 -0600

    SubCategoria - Get - Rutas

--- inv/urls.py
+++ inv/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .views import CategoriaView, CategoriaNew, CategoriaEdit, \
-    CategoriaDel
+    CategoriaDel, \
+        SubCategoriaView
 
 urlpatterns = [
     path('categorias/',CategoriaView.as_view(), name='categoria_list'),
     path('categorias/new',CategoriaNew.as_view(), name='categoria_new'),
     path('categorias/edit/<int:pk>',CategoriaEdit.as_view(), name='categoria_edit'),
     path('categorias/delete/<int:pk>',CategoriaDel.as_view(), name='categoria_del'),
+
+    path('subcategorias/',SubCategoriaView.as_view(), name='subcategoria_list'),
 ]
