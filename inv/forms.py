2760031f3770e289e713e633313c4fa1c7cbd800
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-02 22:51:46 -0600

    SubCategoria - Get - Rutas

--- inv/forms.py
+++ inv/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Categoria
+from .models import Categoria, SubCategoria
 
 
 class CategoriaForm(forms.ModelForm):
@@ -17,3 +17,19 @@ class CategoriaForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class':'form-control'
             })
+
+class SubCategoriaForm(forms.ModelForm):
+    class Meta:
+        model=SubCategoria
+        fields = ['categoria','descripcion','estado']
+        labels = {'descripcion':"Sub Categoría",
+               "estado":"Estado"}
+        widget={'descripcion': forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class':'form-control'
+            })
+        self.fields['categoria'].empty_label =  "Seleccione Categoría"
