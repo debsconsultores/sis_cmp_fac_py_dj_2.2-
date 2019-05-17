fffe0f1e53add6f8ebba1c7cb8dc6d53e3dff15a
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-16 21:25:41 -0600

    Plantilla Sin Privilegios

--- bases/urls.py
+++ bases/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from bases.views import Home
+from bases.views import Home, HomeSinPrivilegios
 
 
 urlpatterns = [
@@ -11,4 +11,8 @@ urlpatterns = [
     path('logout/',
          auth_views.LogoutView.as_view(template_name='bases/login.html'),
          name='logout'),
+
+    path('sin_privilegios/',
+         HomeSinPrivilegios.as_view(),
+         name='sin_privilegios')
 ]
--- bases/views.py
+++ bases/views.py
@@ -7,3 +7,7 @@ from django.views import generic
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url='bases:login'
+
+
+class HomeSinPrivilegios(generic.TemplateView):
+    template_name="bases/sin_privilegios.html"
