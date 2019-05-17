d73afad14ac70f6dff5c1abccc9e00fa228e86a5
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-16 23:00:51 -0600

    Permisos y Login Requerido para Vistas de Funciones

--- inv/views.py
+++ inv/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 from .models import Categoria,SubCategoria, Marca, UnidadMedida, \
     Producto
@@ -135,6 +137,8 @@ class MarcaEdit(LoginRequiredMixin,
         return super().form_valid(form)
 
 
+@login_required(login_url='/login/')
+@permission_required('inv.change_marca', login_url='bases:sin_privilegios')
 def marca_inactivar(request, id):
     marca = Marca.objects.filter(pk=id).first()
     contexto={}
