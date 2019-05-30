d3f9ae58859f58b2b677efb78f41e49cbb8ce6af
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-29 23:35:21 -0600

    Formulario para Encabezado Compra

--- cmp/forms.py
+++ cmp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Proveedor
+from .models import Proveedor, ComprasEnc
 
 class ProveedorForm(forms.ModelForm):
     email = forms.EmailField(max_length=254)
@@ -15,3 +15,25 @@ class ProveedorForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
+
+class ComprasEncForm(forms.ModelForm):
+    fecha_compra = forms.DateInput()
+    fecha_factura = forms.DateInput()
+    
+    class Mega:
+        model=ComprasEnc
+        fields=['proveedor','fecha_compra','observacion',
+            'no_factura','fecha_factura','sub_total',
+            'descuento','total']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['fecha_compra'].widget.attrs['readonly'] = True
+        self.fields['fecha_factura'].widget.attrs['readonly'] = True
+        self.fields['sub_total'].widget.attrs['readonly'] = True
+        self.fields['descuento'].widget.attrs['readonly'] = True
+        self.fields['total'].widget.attrs['readonly'] = True
