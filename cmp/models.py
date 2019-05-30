61c0917177265f89670c78c73da0ff9c819578d7
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-29 22:40:40 -0600

    Modelos y Migración ComprasEnc - ComprasDet

--- cmp/models.py
+++ cmp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from bases.models import ClaseModelo
+from inv.models import Producto
 
 class Proveedor(ClaseModelo):
     descripcion=models.CharField(
@@ -33,3 +34,48 @@ class Proveedor(ClaseModelo):
     class Meta:
         verbose_name_plural = "Proveedores"
 
+
+class ComprasEnc(ClaseModelo):
+    fecha_compra=models.DateField(null=True,blank=True)
+    observacion=models.TextField(blank=True,null=True)
+    no_factura=models.CharField(max_length=100)
+    fecha_factura=models.DateField()
+    sub_total=models.FloatField(default=0)
+    descuento=models.FloatField(default=0)
+    total=models.FloatField(default=0)
+
+    proveedor=models.ForeignKey(Proveedor,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{}'.format(self.observacion)
+
+    def save(self):
+        self.observacion = self.observacion.upper()
+        self.total = self.sub_total - self.descuento
+        super(ComprasEnc,self).save()
+
+    class Meta:
+        verbose_name_plural = "Encabezado Compras"
+        verbose_name="Encabezado Compra"
+
+class ComprasDet(ClaseModelo):
+    compra=models.ForeignKey(ComprasEnc,on_delete=models.CASCADE)
+    producto=models.ForeignKey(Producto,on_delete=models.CASCADE)
+    cantidad=models.BigIntegerField(default=0)
+    precio_prv=models.FloatField(default=0)
+    sub_total=models.FloatField(default=0)
+    descuento=models.FloatField(default=0)
+    total=models.FloatField(default=0)
+    costo=models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.producto)
+
+    def save(self):
+        self.sub_total = float(float(int(self.cantidad)) * float(self.precio_prv))
+        self.total = self.sub_total - float(self.descuento)
+        super(ComprasDet, self).save()
+    
+    class Mega:
+        verbose_name_plural = "Detalles Compras"
+        verbose_name="Detalle Compra"
