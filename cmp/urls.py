bb7e52a0e306ebcb552ec6e3a61767543e3dc7b4
Daniel Bojorge <debs.consultores@gmail.com>
2019-05-29 23:01:38 -0600

    Listar Compras - Vista, Plantilla y URLs

--- cmp/urls.py
+++ cmp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 
 from .views import ProveedorView,ProveedorNew, ProveedorEdit, \
-    proveedorInactivar
+    proveedorInactivar, \
+    ComprasView
 
 urlpatterns = [
     path('proveedores/',ProveedorView.as_view(), name="proveedor_list"),
     path('proveedores/new',ProveedorNew.as_view(), name="proveedor_new"),
     path('proveedores/edit/<int:pk>',ProveedorEdit.as_view(), name="proveedor_edit"),
     path('proveedores/inactivar/<int:id>',proveedorInactivar, name="proveedor_inactivar"),
+
+    path('compras/',ComprasView.as_view(), name="compras_list"),
 ]
--- cmp/views.py
+++ cmp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
 import json
 
-from .models import Proveedor
+from .models import Proveedor, ComprasEnc, ComprasDet
 from cmp.forms import ProveedorForm
 from bases.views import SinPrivilegios
 
@@ -69,3 +69,10 @@ def proveedorInactivar(request,id):
         return HttpResponse('Proveedor Inactivado')
 
     return render(request,template_name,contexto)
+
+
+class ComprasView(SinPrivilegios, generic.ListView):
+    model = ComprasEnc
+    template_name = "cmp/compras_list.html"
+    context_object_name = "obj"
+    permission_required="cmp.view_comprasenc"
