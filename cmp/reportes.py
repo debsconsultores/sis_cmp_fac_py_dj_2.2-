107be9a4d870542039ca6468381999843b9b374d
Daniel Bojorge <debs.consultores@gmail.com>
2019-06-03 22:57:07 -0600

    Imprimir Una Compras - Vista

--- cmp/reportes.py
+++ cmp/reportes.py
@@ -59,3 +59,38 @@ def reporte_compras(request):
     if pisaStatus.err:
        return HttpResponse('We had some errors <pre>' + html + '</pre>')
     return response
+
+
+def imprimir_compra(request, compra_id):
+    template_path = 'cmp/compras_print_one.html'
+    today = timezone.now()
+    
+    enc = ComprasEnc.objects.filter(id==compra_id)
+    if enc:
+        detalle = ComprasDet.objects.filter(compra__id=compra_id)
+    else:
+        detalle={}
+
+    
+    context = {
+        'detalle': detalle,
+        'encabezado':enc,
+        'today':today,
+        'request': request
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisaStatus = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisaStatus.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
