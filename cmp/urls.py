2b00f1855f38e6aa3fbec2d5e0339e24670df03f
Daniel Bojorge <debs.consultores@gmail.com>
2019-06-03 22:35:39 -0600

    Imprimir Todas las Compras - Ruta

--- cmp/urls.py
+++ cmp/urls.py
@@ -4,6 +4,8 @@ from .views import ProveedorView,ProveedorNew, ProveedorEdit, \
     proveedorInactivar, \
     ComprasView, compras, CompraDetDelete
 
+from .reportes import reporte_compras
+
 urlpatterns = [
     path('proveedores/',ProveedorView.as_view(), name="proveedor_list"),
     path('proveedores/new',ProveedorNew.as_view(), name="proveedor_new"),
@@ -14,4 +16,6 @@ urlpatterns = [
     path('compras/new',compras, name="compras_new"),
     path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
     path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
+
+    path('compras/listado', reporte_compras, name='compras_print_all'),
 ]
