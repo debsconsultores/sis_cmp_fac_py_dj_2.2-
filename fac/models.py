2178e36b165de69da1b1cb4912307965d4825472
Daniel Bojorge <debs.consultores@gmail.com>
2019-07-04 22:49:45 -0600

    Sección 13 - Modelo Cliente

--- fac/models.py
+++ fac/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo
+
+class Cliente(ClaseModelo):
+    NAT='Natural'
+    JUR='Jurídica'
+    TIPO_CLIENTE = [
+        (NAT,'Natural'),
+        (JUR,'Jurídica')
+    ]
+    nombres = models.CharField(
+        max_length=100
+    )
+    apellidos = models.CharField(
+        max_length=100
+    )
+    celular = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    tipo=models.CharField(
+        max_length=10,
+        choices=TIPO_CLIENTE,
+        default=NAT
+    )
+
+    def __str__(self):
+        return '{} {}'.format(self.apellidos,self.nombres)
+
+    def save(self):
+        self.nombres = self.nombres.upper()
+        self.apellidos = self.apellidos.upper()
+        super(Cliente, self).save()
+
+    class Meta:
+        verbose_name_plural = "Clientes"
+
+    
+    
+    
