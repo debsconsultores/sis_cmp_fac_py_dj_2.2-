7667dbe6ec0a58330fa90bf3f0e4abb9ac39fe21
Daniel Bojorge <debs.consultores@gmail.com>
2019-07-04 23:03:14 -0600

    Sección 13 - Listar Cliente

--- fac/urls.py
+++ fac/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from .views import ClienteView
+
 urlpatterns = [
+    path('clientes/',ClienteView.as_view(), name="cliente_list"),
 
 ]
--- fac/views.py
+++ fac/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from bases.views import SinPrivilegios
+from .models import Cliente
+
+class ClienteView(SinPrivilegios, generic.ListView):
+    model = Cliente
+    template_name = "fac/cliente_list.html"
+    context_object_name = "obj"
+    permission_required="cmp.view_cliente"
