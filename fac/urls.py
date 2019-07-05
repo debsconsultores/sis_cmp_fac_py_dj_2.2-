0d2b8e4e2f7897ed8f4326e929ec14bf3d12f4b0
Daniel Bojorge <debs.consultores@gmail.com>
2019-07-04 23:36:27 -0600

    Sección 13 - Herencia de Vistas para New y Edit Cliente

--- fac/urls.py
+++ fac/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
-from .views import ClienteView
+from .views import ClienteView,ClienteNew,ClienteEdit
 
 urlpatterns = [
     path('clientes/',ClienteView.as_view(), name="cliente_list"),
+    path('clientes/new',ClienteNew.as_view(), name="cliente_new"),
+    path('clientes/<int:pk>',ClienteEdit.as_view(), name="cliente_edit"),
+
 
 ]
--- fac/views.py
+++ fac/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
 from django.views import generic
 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+
 from bases.views import SinPrivilegios
+
 from .models import Cliente
+from .forms import ClienteForm
 
 class ClienteView(SinPrivilegios, generic.ListView):
     model = Cliente
     template_name = "fac/cliente_list.html"
     context_object_name = "obj"
     permission_required="cmp.view_cliente"
+
+
+class VistaBaseCreate(SuccessMessageMixin,SinPrivilegios, \
+    generic.CreateView):
+    context_object_name = 'obj'
+    success_message="Registro Agregado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class VistaBaseEdit(SuccessMessageMixin,SinPrivilegios, \
+    generic.UpdateView):
+    context_object_name = 'obj'
+    success_message="Registro Actualizado Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+
+class ClienteNew(VistaBaseCreate):
+    model=Cliente
+    template_name="fac/cliente_form.html"
+    form_class=ClienteForm
+    success_url= reverse_lazy("fac:cliente_list")
+    permission_required="fac.add_cliente"
+
+
+class ClienteEdit(VistaBaseEdit):
+    model=Cliente
+    template_name="fac/cliente_form.html"
+    form_class=ClienteForm
+    success_url= reverse_lazy("fac:cliente_list")
+    permission_required="fac.change_cliente"
