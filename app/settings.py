881ade62d50d144c1e59d32c5231873fd986a38e
Daniel Bojorge <debs.consultores@gmail.com>
2019-07-19 21:52:56 -0600

    Sección 13 - Nueva Clase Modelo - Mejora Usuario Crea y Modifica

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'inv',
     'cmp',
     'fac',
+    'django_userforeignkey',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_userforeignkey.middleware.UserForeignKeyMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
--- bases/models.py
+++ bases/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django_userforeignkey.models.fields import UserForeignKey
 
 
 class ClaseModelo(models.Model):
@@ -12,4 +13,18 @@ class ClaseModelo(models.Model):
 
     class Meta:
         abstract=True
+
+
+class ClaseModelo2(models.Model):
+    estado = models.BooleanField(default=True)
+    fc = models.DateTimeField(auto_now_add=True)
+    fm = models.DateTimeField(auto_now=True)
+    # uc = models.ForeignKey(User, on_delete=models.CASCADE)
+    # um = models.IntegerField(blank=True,null=True)
+    uc = UserForeignKey(auto_user_add=True,related_name='+')
+    um = UserForeignKey(auto_user=True,related_name='+')
+
+    class Meta:
+        abstract=True
+
 
