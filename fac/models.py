63eaacbd7cb5bb6b8e09ff93860c76e7c6806ce1
Daniel Bojorge <debs.consultores@gmail.com>
2019-07-19 22:07:28 -0600

    Sección 13 - Modelos Encabezado y Detalle para Facturación

--- fac/models.py
+++ fac/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from bases.models import ClaseModelo
+from bases.models import ClaseModelo, ClaseModelo2
+from inv.models import Producto
 
 class Cliente(ClaseModelo):
     NAT='Natural'
@@ -38,5 +39,43 @@ class Cliente(ClaseModelo):
         verbose_name_plural = "Clientes"
 
     
+class FacturaEnc(ClaseModelo2):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    sub_total=models.FloatField(default=0)
+    descuento=models.FloatField(default=0)
+    total=models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def save(self):
+        self.total = self.sub_total - self.descuento
+        super(FacturaEnc,self).save()
+
+    class Meta:
+        verbose_name_plural = "Encabezado Facturas"
+        verbose_name="Encabezado Factura"
     
+
+class FacturaDet(ClaseModelo2):
+    factura = models.ForeignKey(FacturaEnc,on_delete=models.CASCADE)
+    producto=models.ForeignKey(Producto,on_delete=models.CASCADE)
+    cantidad=models.BigIntegerField(default=0)
+    precio=models.FloatField(default=0)
+    sub_total=models.FloatField(default=0)
+    descuento=models.FloatField(default=0)
+    total=models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.producto)
+
+    def save(self):
+        self.sub_total = float(float(int(self.cantidad)) * float(self.precio))
+        self.total = self.sub_total - float(self.descuento)
+        super(FacturaDet, self).save()
+    
+    class Meta:
+        verbose_name_plural = "Detalles Facturas"
+        verbose_name="Detalle Factura"
     
