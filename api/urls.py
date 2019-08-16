ba8650332cf91e893346dcf2b4772287c7a3f152
Daniel Bojorge <debs.consultores@gmail.com>
2019-08-16 00:32:14 -0600

    Sección 13 - EndPoint Productos - View

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from .views import ProductoList, ProductoDetalle
 
 urlpatterns = [
+    path('v1/productos/',ProductoList.as_view(),name='producto_list'),
+    path('v1/productos/<str:codigo>',ProductoDetalle.as_view(),name='producto_detalle'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .serializers import ProductoSerializer
+from inv.models import Producto
+
+class ProductoList(APIView):
+    def get(self,request):
+        prod = Producto.objects.all()
+        data = ProductoSerializer(prod,many=True).data
+        return Response(data)
+
+
+class ProductoDetalle(APIView):
+    def get(self,request, codigo):
+        prod = get_object_or_404(Producto,codigo=codigo)
+        data = ProductoSerializer(prod).data
+        return Response(data)
