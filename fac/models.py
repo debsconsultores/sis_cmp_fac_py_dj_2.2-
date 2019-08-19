122424e6d633e64762052d18c2a64164286c6ade
Daniel Bojorge <debs.consultores@gmail.com>
2019-08-19 00:13:49 -0600

    Sección 13 - Calcular Totales al agregar detalle con Signals

--- fac/models.py
+++ fac/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+#Para los signals
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.db.models import Sum
+
 from bases.models import ClaseModelo, ClaseModelo2
 from inv.models import Producto
 
@@ -78,4 +83,34 @@ class FacturaDet(ClaseModelo2):
     class Meta:
         verbose_name_plural = "Detalles Facturas"
         verbose_name="Detalle Factura"
-    
+
+
+@receiver(post_save, sender=FacturaDet)
+def detalle_fac_guardar(sender,instance,**kwargs):
+    factura_id = instance.factura.id
+    producto_id = instance.producto.id
+
+    enc = FacturaEnc.objects.get(pk=factura_id)
+    if enc:
+        sub_total = FacturaDet.objects \
+            .filter(factura=factura_id) \
+            .aggregate(sub_total=Sum('sub_total')) \
+            .get('sub_total',0.00)
+        
+        descuento = FacturaDet.objects \
+            .filter(factura=factura_id) \
+            .aggregate(descuento=Sum('descuento')) \
+            .get('descuento',0.00)
+        
+        enc.sub_total = sub_total
+        enc.descuento = descuento
+        enc.save()
+
+    prod=Producto.objects.filter(pk=producto_id).first()
+    if prod:
+        cantidad = int(prod.existencia) - int(instance.cantidad)
+        prod.existencia = cantidad
+        prod.save()
+
+
+
