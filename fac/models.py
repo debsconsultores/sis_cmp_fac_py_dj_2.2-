c173e5d0647d4305a3786563c9d7b361ce27d3e8
Daniel Bojorge <debs.consultores@gmail.com>
2019-08-31 22:58:00 -0600

    Sección 13 - Permisos Personalizados

--- fac/models.py
+++ fac/models.py
@@ -61,6 +61,9 @@ class FacturaEnc(ClaseModelo2):
     class Meta:
         verbose_name_plural = "Encabezado Facturas"
         verbose_name="Encabezado Factura"
+        permissions = [
+            ('sup_caja_facturaenc','Permisos de Supervisor de Caja Encabezado')
+        ]
     
 
 class FacturaDet(ClaseModelo2):
@@ -83,6 +86,9 @@ class FacturaDet(ClaseModelo2):
     class Meta:
         verbose_name_plural = "Detalles Facturas"
         verbose_name="Detalle Factura"
+        permissions = [
+            ('sup_caja_facturadet','Permisos de Supervisor de Caja Detalle')
+        ]
 
 
 @receiver(post_save, sender=FacturaDet)
